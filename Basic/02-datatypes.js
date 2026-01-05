7a27f89927b7631e9b948dd866a7ee845af6db86
Alberto <cruzcorreaalberto15@gmail.com>
2026-01-05 12:38:09 -0500

    update filds in basic

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,32 @@
+// 7 tipos de datos primitivos //
+
+//String: Cadena de texto
+let name = "Betico"
+let alias = 'Betico777'
+let email = `cruzcorreaalberto15@gmail.com`
+
+//Number
+let age = 37 // entero
+let height = 1.78  //decimal
+
+//Boolean
+let isStudent = true
+let isTeacher = false
+
+// Undefined
+
+let undefinedValue 
+console.log(undefinedValue)
+
+// Null nos dice q intencionadamente esta variable es nula
+
+let nullValue = null
+
+// Symbol
+
+let mySymbol= Symbol("mysymbol")
+
+//BigInt para representar un numero extremadamente grande ya q tenemos reservados 64 bits en el tipo numero
+
+let myBigInt = BigInt("6756866457476768768576987684769847687569847698")
+let myBigInt2 = 6756866457476768768576987684769847687569847698n
